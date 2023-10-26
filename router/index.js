a789680f95d19857148dee02c537e6156621acfe
kakage553656552 <Kaka.zhang@mspbots.ai>
2023-10-26 17:50:08 +0800

    test

--- router/index.js
+++ router/index.js
@@ -1,76 +1,99 @@
-const {generateRandomString} = require('./random.js');
 const mysql = require('mysql');
-const connection = mysql.createConnection({
+const util = require('util');
+const { generateRandomString } = require('./random.js');
+
+const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
   password: 'root',
-  database: 'mysql'
+  database: 'mysql',
 });
-connection.connect();
+
+const query = util.promisify(pool.query).bind(pool);
+
 module.exports = function (app) {
-    var tokenList = []
-    app.get('/test', function (req, res) {
-        connection.query('SELECT * from sys_user', function (error, results, fields) {
-            if (error) throw error;
-            var result = {
-                code:0,
-                msg:'success',
-                data:results
-            }
-            res.send(result)
-        });
-    });
-    app.post('/login', function (req, res) {
-        connection.query(`select * from sys_user where name = "${req.body.name}" and password = ${req.body.password}`, function (error, results, fields) {
-            if (error) throw error;
-            var result = {
-                code:0,
-                msg:'success'
-            }
-            if(results.length===0) {
-                result.code = 500,
-                result.msg='no data'
-            }else {
-                result.token = generateRandomString(36)
-                // tokenList.push(result.token)
-                connection.query(`update sys_user set token = "${result.token}" where id=${results[0].id}`, function (error, results, fields) {
-                  console.log(11,error,222,results)
-                  if (error) throw error;
-                });
-            }
-            res.send(result)
-        });
-    });
-    app.post('/insert', function (req, res) {
-        connection.query(`insert into sys_user (name,password) values("${req.body.name}",${req.body.password})`, function (error, results, fields) {
-            if (error) throw error;
-            var result = {
-                code:0,
-                msg:'success'
-            }
-            res.send(result)
-        });
-    });
-    app.post('/update', function (req, res) {
-        connection.query(`update sys_user set name = "${req.body.name}",password="${req.body.password}" where id=${req.body.id}`, function (error, results, fields) {
-            if (error) throw error;
-            var result = {
-                code:0,
-                msg:'success'
-            }
-            res.send(result)
-        });
-    });
-    app.delete('/delete', function (req, res) {
-        var url = require('url')
-        var parseObj = url.parse(req.url,true)
-        connection.query(`delete from sys_user where id = ${parseObj.query.id}`, function (error, results, fields) {
-            if (error) throw error;
-            var result = {
-                code:0,
-                msg:'success'
-            }
-            res.send(result)
-        });
-    });
-}
+  app.get('/test', async (req, res) => {
+    try {
+      const results = await query('SELECT * from sys_user');
+      const result = {
+        code: 0,
+        msg: 'success',
+        data: results,
+      };
+      res.send(result);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Server Error');
+    }
+  });
+
+  app.post('/login', async (req, res) => {
+    try {
+      const results = await query(
+        'SELECT * FROM sys_user WHERE name = ? AND password = ?',
+        [req.body.name, req.body.password]
+      );
+
+      const result = {
+        code: 0,
+        msg: 'success',
+      };
+
+      if (results.length === 0) {
+        result.code = 500;
+        result.msg = 'no data';
+      } else {
+        result.token = generateRandomString(36);
+        await query('UPDATE sys_user SET token = ? WHERE id = ?', [result.token, results[0].id]);
+      }
+
+      res.send(result);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Server Error');
+    }
+  });
+
+  app.post('/insert', async (req, res) => {
+    try {
+      await query('INSERT INTO sys_user (name, password) VALUES (?, ?)', [req.body.name, req.body.password]);
+      const result = {
+        code: 0,
+        msg: 'success',
+      };
+      res.send(result);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Server Error');
+    }
+  });
+
+  app.post('/update', async (req, res) => {
+    try {
+      await query('UPDATE sys_user SET name = ?, password = ? WHERE id = ?', [req.body.name, req.body.password, req.body.id]);
+      const result = {
+        code: 0,
+        msg: 'success',
+      };
+      res.send(result);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Server Error');
+    }
+  });
+
+  app.delete('/delete', async (req, res) => {
+    const id = req.query.id;
+    try {
+      await query('DELETE FROM sys_user WHERE id = ?', [id]);
+      const result = {
+        code: 0,
+        msg: 'success',
+      };
+      res.send(result);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Server Error');
+    }
+  });
+};
